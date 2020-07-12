4c9abebc425ea46f30978ef7f2de5cafb9506c53
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2020-07-12 14:02:50 -0700

    Menambahkan file html dan variableJs

---
+++ 01_variable_sigit.js
@@ -0,0 +1,13 @@
+// variable yang bisa di assigment
+
+var a = 'a';
+console.log(a);
+
+let x = 'aku';
+x = 'kamu';
+console.log(x);
+
+//  variable yang tidak bisa di assignment
+
+const y = 'x';
+console.log(y);
